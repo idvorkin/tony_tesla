3ad598f9732f76143e28d8546e49b6a6ae942da1
Igor Dvorkin <idvorkin@gmail.com>
2024-09-10 09:07:54 -0700

    Checkpoint bus.py

--- bus.py
+++ bus.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
 
 
+from __future__ import annotations
 import csv
 import json
-import os
-from datetime import datetime, timedelta
+from datetime import datetime
 from functools import lru_cache
-from pathlib import Path
-from uuid import uuid4
 import pytz
 
-import dateutil.parser
 import httpx
 import typer
 from icecream import ic
 from loguru import logger
 from pydantic import BaseModel
-from typing import Optional
 from rich.console import Console
+import asyncio
 
 console = Console()
 app = typer.Typer(no_args_is_help=True)
@@ -46,30 +43,31 @@ class Stops(BaseModel):
     stop_url: str
     location_type: str
 
-class Trip(BaseModel):
-    trip_id: str
-    route_id: str
-    direction_id: int
-
-class Arrival(BaseModel):
-    time: int
-
-class Departure(BaseModel):
-    time: int
-
-class StopTimeUpdate(BaseModel):
-    stop_id: str
-    arrival: Arrival | None  = None
-    departure: Departure | None = None
-    schedule_relationship: str
-
-class Vehicle(BaseModel):
-    id: str
-    label: str
 
 class TripUpdate(BaseModel):
-    trip: Trip
-    stop_time_update: list[StopTimeUpdate] 
+    class Trip(BaseModel):
+        trip_id: str
+        route_id: str
+        direction_id: int
+
+    class Arrival(BaseModel):
+        time: int
+
+    class Departure(BaseModel):
+        time: int
+
+    class StopTimeUpdate(BaseModel):
+        stop_id: str
+        arrival: TripUpdate.Arrival | None = None
+        departure: TripUpdate.Departure | None = None
+        schedule_relationship: str
+
+    class Vehicle(BaseModel):
+        id: str
+        label: str
+
+    trip: TripUpdate.Trip
+    stop_time_update: list[StopTimeUpdate]
     vehicle: Vehicle | None = None
 
 
@@ -89,26 +87,32 @@ def get_routes():
 
     # loads the routes.csv file into a dict of Routes
     routes = {}
-    with open('routes.txt', 'r') as file:
+    with open("routes.txt", "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            routes[row['route_short_name']] = Routes(**row)
+            routes[row["route_short_name"]] = Routes(**row)
     return routes
+
 
 @lru_cache
 def get_trip_updates():
     """Load and return trip updates from tripupdates_pb.json file."""
     trip_updates = []
-    with open('tripupdates_pb.json', 'r') as file:
+    with open("tripupdates_pb.json", "r") as file:
         entities = json.load(file)["entity"]
-        trip_updates = [entity['trip_update']for entity in entities if entity['trip_update'] is not None]  
+        trip_updates = [
+            entity["trip_update"]
+            for entity in entities
+            if entity["trip_update"] is not None
+        ]
         # convert each trip_update to a TripUpdate
         # only load entities which are trip_updates
         ic(trip_updates[0])
         trip_updates = [TripUpdate(**trip_update) for trip_update in trip_updates]
-        
+
     ic(len(trip_updates))
     return trip_updates
+
 
 @lru_cache
 def get_stops():
@@ -125,55 +129,62 @@ def get_stops():
 
     # loads the stops.txt file into a dict of Stops
     stops = {}
-    with open('stops.txt', 'r', encoding='utf-8') as file:
+    with open("stops.txt", "r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         for row in reader:
             # if there is a failure in a row, skip it
-            stop_id = row['stop_id']
+            stop_id = row["stop_id"]
             stops[stop_id] = Stops(**row)
 
     return stops
+
 
 @app.command()
 def stops_for_route(route: str):
     route_id = get_routes()[route].route_id
     trip_updates = get_trip_updates()
     for trip_update in trip_updates:
-        # do it trip by trip, 
+        # do it trip by trip,
         trips = [trip for trip in trip_updates if trip.trip.route_id == route_id]
         for trip in trips:
-            print("# TRIP: ", trip.trip.trip_id, trip.trip.direction_id, trip.trip.)
+            print("# TRIP: ", trip.trip.trip_id, trip.trip.direction_id)
             for stop_time_update in trip.stop_time_update:
                 stop_id = stop_time_update.stop_id
                 stop = get_stops()[stop_id]
                 stop_name = stop.stop_name
                 arrival_time = stop_time_update.arrival.time
-                arrival_time_pst = datetime.fromtimestamp(arrival_time).astimezone(pytz.timezone('US/Pacific'))
+                arrival_time_pst = datetime.fromtimestamp(arrival_time).astimezone(
+                    pytz.timezone("US/Pacific")
+                )
                 # make arrivat time just 07:43 pm
-                arrival_time_pst = arrival_time_pst.strftime('%I:%M %p')
-                # include the direction_id  
+                arrival_time_pst = arrival_time_pst.strftime("%I:%M %p")
+                # include the direction_id
                 direction_id = trip_update.trip.direction_id
                 print(f"{stop_name} - {arrival_time_pst} - {direction_id}")
 
 
 @app.command()
 def get_latest_data():
-    import asyncio
-
-    import httpx
-
     async def download_file(url, filename):
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
             if response.status_code == 200:
-                with open(filename, 'wb') as file:
+                with open(filename, "wb") as file:
                     file.write(response.content)
             else:
-                logger.error(f"Failed to download {filename}. Status code: {response.status_code}")
+                logger.error(
+                    f"Failed to download {filename}. Status code: {response.status_code}"
+                )
 
     urls = [
-        ("https://s3.amazonaws.com/kcm-alerts-realtime-prod/tripupdates_pb.json", "tripupdates_pb.json"),
-        ("https://s3.amazonaws.com/kcm-alerts-realtime-prod/tripupdates_enhanced.json", "tripupdates_enhanced.json")
+        (
+            "https://s3.amazonaws.com/kcm-alerts-realtime-prod/tripupdates_pb.json",
+            "tripupdates_pb.json",
+        ),
+        (
+            "https://s3.amazonaws.com/kcm-alerts-realtime-prod/tripupdates_enhanced.json",
+            "tripupdates_enhanced.json",
+        ),
     ]
 
     async def main():
@@ -181,7 +192,6 @@ def get_latest_data():
         await asyncio.gather(*tasks)
 
     asyncio.run(main())
-
 
 
 @logger.catch()
