6e7cdeac14b9b5b000bc3f95ec9c3c180d48f02a
Igor Dvorkin <idvorkin@gmail.com>
2024-09-14 12:04:40 -0700

    Update

--- bus.py
+++ bus.py
@@ -3,8 +3,7 @@
 
 import csv
 import json
-import os
-from datetime import datetime, timedelta
+from datetime import datetime
 from functools import lru_cache
 from pathlib import Path
 
@@ -22,7 +21,6 @@ from zoneinfo import ZoneInfo
 
 console = Console()
 app = typer.Typer(no_args_is_help=True)
-
 
 
 class Routes(BaseModel):
@@ -48,55 +46,68 @@ class Stops(BaseModel):
     stop_url: str
     location_type: str
 
+
 class Trip(BaseModel):
     trip_id: str
     route_id: str
     direction_id: int
 
+
 class Arrival(BaseModel):
     time: int
+
 
 class Departure(BaseModel):
     time: int
 
+
 class StopTimeUpdate(BaseModel):
     stop_id: str
-    arrival: Arrival | None  = None
+    arrival: Arrival | None = None
     departure: Departure | None = None
     schedule_relationship: str
+
 
 class Vehicle(BaseModel):
     id: str
     label: str
 
+
 class TripUpdate(BaseModel):
     trip: Trip
-    stop_time_update: list[StopTimeUpdate] 
+    stop_time_update: list[StopTimeUpdate]
     vehicle: Vehicle | None = None
 
 
 # Set the root directory for all file operations
 transit_path = Path("modal_readonly/transit")
 
+
 # make this cached
 @lru_cache
 def get_routes():
     """Load and return routes from routes.txt file."""
     routes = {}
-    with (transit_path / 'routes.txt').open('r', encoding='utf-8') as file:
+    with (transit_path / "routes.txt").open("r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            routes[row['route_short_name']] = Routes(**row)
+            routes[row["route_short_name"]] = Routes(**row)
     return routes
+
 
 @lru_cache
 def get_trip_updates():
     """Load and return trip updates from tripupdates_pb.json file."""
-    with (transit_path / 'tripupdates_pb.json').open('r') as file:
+    with (transit_path / "tripupdates_pb.json").open("r") as file:
         entities = json.load(file)["entity"]
-        trip_updates = [entity['trip_update'] for entity in entities if entity['trip_update'] is not None]
+        trip_updates = [
+            entity["trip_update"]
+            for entity in entities
+            if entity["trip_update"] is not None
+        ]
         trip_updates = [TripUpdate(**trip_update) for trip_update in trip_updates]
     return trip_updates
+
 
 @lru_cache
 def get_stops():
@@ -113,21 +124,26 @@ def get_stops():
 
     # loads the stops.txt file into a dict of Stops
     stops = {}
-    with (transit_path / 'stops.txt').open('r', encoding='utf-8') as file:
+    with (transit_path / "stops.txt").open("r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         for row in reader:
             # if there is a failure in a row, skip it
-            stop_id = row['stop_id']
+            stop_id = row["stop_id"]
             stops[stop_id] = Stops(**row)
 
     return stops
+
 
 @app.command()
 def stops_for_route(route: str):
     route_id = get_routes()[route].route_id
     trip_updates = get_trip_updates()
     the_route = get_routes()[route]
-    tripupdates_for_route = [trip for trip in trip_updates if trip.trip.route_id == route_id and trip.stop_time_update]
+    tripupdates_for_route = [
+        trip
+        for trip in trip_updates
+        if trip.trip.route_id == route_id and trip.stop_time_update
+    ]
     ic(len(tripupdates_for_route))
     # get the route+direction with the most stops
 
@@ -150,45 +166,63 @@ def stops_for_route(route: str):
             if not stop_time_update.arrival:
                 continue
             arrival_time = stop_time_update.arrival.time
-            arrival_time_pst = datetime.fromtimestamp(arrival_time, tz=ZoneInfo("America/Los_Angeles"))
+            arrival_time_pst = datetime.fromtimestamp(
+                arrival_time, tz=ZoneInfo("America/Los_Angeles")
+            )
             # make arrivat time just 07:43 pm
-            arrival_time_pst = arrival_time_pst.strftime('%I:%M %p')
-            # include the direction_id  
+            arrival_time_pst = arrival_time_pst.strftime("%I:%M %p")
+            # include the direction_id
             direction_id = trip.trip.direction_id
             print(f"{stop_name} - {arrival_time_pst} - {direction_id} - {stop_id}")
 
+
 @app.command()
 def library():
-    client = onebusaway.OnebusawaySDK(
-    api_key=os.environ.get("ONEBUSAWAY_API_KEY"),
-)
+    client = onebusaway.OnebusawaySDK()
+    # r2 = client.stops_for_route.list("1_100228")
+    # stops = r2.data.entry
+    # ic(stops)
+
+    # schedule_for_stop_response = client.schedule_for_stop.retrieve(library_stop_id)
+    # schedule_for_stop = schedule_for_stop_response.data.entry
+    # for stop_route_schedule in schedule_for_stop.stop_route_schedules:
+    # print(stop_route_schedule.stop_route_direction_schedules)
+    # print(stop_route_schedule.model_dump_json(indent=2))
+
     library_stop_id = "1_29249"
-    response = client.arrival_and_departure.list(library_stop_id)
-    trips = response.data.entry.arrivals_and_departures
-    for trip in trips:
-        at_time = trip.predicted_arrival_time / 1000
+    library_arrivals_response = client.arrival_and_departure.list(library_stop_id)
+    library_arrivals = library_arrivals_response.data.entry.arrivals_and_departures
+    library_arrivals.sort(key=lambda x: x.predicted_arrival_time, reverse=True)
+    for arrival in library_arrivals:
+        at_time = arrival.predicted_arrival_time / 1000
         at_time_pst = datetime.fromtimestamp(at_time)
-        at_time_pst = at_time_pst.strftime('%I:%M %p')
-        print(trip.trip_headsign)
-        print(at_time_pst)
-        #ic(trip)
+        at_time_pst = at_time_pst.strftime("%I:%M %p")
+        print(f" {at_time_pst} - {arrival.route_short_name} to {arrival.trip_headsign}")
+        # ic(trip)
 
 
 @app.command()
 def get_latest_data():
-
     async def download_file(url, filename):
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
             if response.status_code == 200:
-                with open(transit_path / filename, 'wb') as file:
+                with open(transit_path / filename, "wb") as file:
                     file.write(response.content)
             else:
-                logger.error(f"Failed to download {filename}. Status code: {response.status_code}")
+                logger.error(
+                    f"Failed to download {filename}. Status code: {response.status_code}"
+                )
 
     urls = [
-        ("https://s3.amazonaws.com/kcm-alerts-realtime-prod/tripupdates_pb.json", "tripupdates_pb.json"),
-        ("https://s3.amazonaws.com/kcm-alerts-realtime-prod/tripupdates_enhanced.json", "tripupdates_enhanced.json")
+        (
+            "https://s3.amazonaws.com/kcm-alerts-realtime-prod/tripupdates_pb.json",
+            "tripupdates_pb.json",
+        ),
+        (
+            "https://s3.amazonaws.com/kcm-alerts-realtime-prod/tripupdates_enhanced.json",
+            "tripupdates_enhanced.json",
+        ),
     ]
 
     async def main():
@@ -197,8 +231,8 @@ def get_latest_data():
         await asyncio.gather(*tasks)
         end = time.time()
         print(f"Time taken: {end - start} seconds")
-    asyncio.run(main())
 
+    asyncio.run(main())
 
 
 @logger.catch()
